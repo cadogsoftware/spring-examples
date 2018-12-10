da25112121c0f9b4476c46b6e495b587dfd1868a
Richard Morgan <richard@cadogsoftware.co.uk>
2018-12-10 08:37:48 +0000

    Start adding simple Spring Controllers for Books and Authors

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -35,10 +35,6 @@ public class Author {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getFirstName() {
         return firstName;
     }
